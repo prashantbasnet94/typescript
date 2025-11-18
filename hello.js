17cc0dae9ab5291328232816f8ff2d0b1c708c73
Prashant Basnet <30943807+prashantbasnet94@users.noreply.github.com>
2025-11-18 12:37:46 -0600

    Type annotations never change the runtime behavior of your program.

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
+//it needs some way to strip out or transform any TypeScript-specific code so that you can run it. 
+//Most TypeScript-specific code gets erased away, and likewise, here our type annotations were completely erased.
 function greet(person, date) {
     console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
 }
