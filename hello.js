9a345efaaec6ed07faa73285784f2a2853c16d9a
Prashant Basnet <30943807+prashantbasnet94@users.noreply.github.com>
2025-11-18 12:40:31 -0600

    downleveling

--- hello.js
+++ hello.js
@@ -1,6 +1,13 @@
-//it needs some way to strip out or transform any TypeScript-specific code so that you can run it. 
-//Most TypeScript-specific code gets erased away, and likewise, here our type annotations were completely erased.
+/*
+TypeScript has the ability to rewrite code from newer versions of ECMAScript to older ones such as ECMAScript 3 or ECMAScript 5 (a.k.a. ES5).
+
+1. By default TypeScript targets ES5, an extremely old version of ECMAScript. 
+2. We could have chosen something a little bit more recent by using the target option.
+
+This process of moving from a newer or “higher” version of ECMAScript down to an older or “lower” one is sometimes called downleveling.
+
+*/
 function greet(person, date) {
-    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
 greet("Maddison", new Date());
